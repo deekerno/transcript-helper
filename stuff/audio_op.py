49aed25b531c5c439529f416852ed923f7b30056
Alexander deCurnou <adecurnou@gmail.com>
2017-02-16 00:26:49 -0500

    Added audio stuff and fixed errors in project class

---
+++ stuff/audio_op.py
@@ -0,0 +1,45 @@
+from math import ceil
+from moviepy.editor import VideoFileClip, AudioFileClip
+from os.path import splitext, join
+
+DEFAULT_VIDEO_AUDIO_CODEC = 'libmp3lame'
+DEFAULT_AUDIOFILE_CODEC = 'pcm_s16le' # 16-bit WAV
+DEFAULT_AUDIOFILE_BITRATE = '16k'
+DEFAULT_ZEROES_PADDING = 5
+DEFAULT_AUDIO_SEGMENT_DURATION_SEC = 300
+
+def audio_extraction(vid_src, audio_dest, audio_codec=DEFAULT_AUDIOFILE_CODEC,
+				audio_bitrate=DEFAULT_AUDIOFILE_BITRATE):
+	try:
+		video = VideoFileClip(vid_src)
+		audio = video.audio
+		audio.write_audiofile(audio_dest, codec=audio_codec, bitrate=audio_bitrate)
+		print("Audio file extracted.")
+	except:
+		print("Unexpected error!")
+		raise
+
+def audio_segmentation(audio_src, audio_seg_dir,
+					seg_dur=DEFAULT_AUDIO_SEGMENT_DURATION_SEC,
+					pad_zeroes=DEFAULT_ZEROES_PADDING):
+	src_basename, src_ext = splitext(audio_src)
+	audio = AudioFileClip(audio_src)
+	total_sec = audio.duration
+	start_sec = 0
+
+	while start_sec < total_sec:
+		end_sec = start_sec + seg_dur
+		if end_sec > total_sec:
+			end_sec = ceil(total_sec)
+			segment = audio.subclip(start_sec)
+		else:
+			segment = audio.subclip(start_sec, end_sec)
+		seg_name = "%s-%s%s" % (
+			str(start_sec).rjust(pad_zeroes, "0"),
+			str(end_sec).rjust(pad_zeroes, "0"), src_ext)
+		start_sec = end_sec
+		seg_full_path = join(audio_seg_dir, seg_name)
+		segment.write_audiofile(seg_full_path)
+		yield seg_full_path
+
+	print("Audio segmentation complete.")
--- stuff/project.py
+++ stuff/project.py
@@ -1,6 +1,6 @@
 from glob import glob
 from os import makedirs
-from os.path import join, basename, splitext, expanduser, abspath, isdir
+from os.path import join, basename, splitext, expanduser, abspath
 import json
 
 PROJECTS_DIR = join(".", "projects")
@@ -11,29 +11,29 @@ WORDS_TRANSCRIPT_BASENAME = 'words-transcript.csv'
 
 class Project:
 	# Set the project slug to the filename of the video
-	def __init__(self, path_to_vid):
-		self.slug = splitext(basename(path_to_vid))[0]
+	def __init__(self, filename):
+		self.slug = splitext(basename(filename))[0]
 
-	def make_proj_dir():
-	"""
-	This will take the project's slug and expand to the full path. It will then
-	attempt to create a directory for the project. exist_ok is set to True as 
-	it will not wipe the directory if it already exists, which is fine in my case
-	""" 
+	def make_proj_dir(self):
+		"""
+		This will take the project's slug and expand to the full path. It will
+		then attempt to create a directory for the project. exist_ok is set to
+		True as it will not wipe the directory if it already exists, which is 
+		fine in my case
+		""" 
 		xslug = self.slug.replace("projects/", "").rstrip('/')
 		try:
 			self.path = join(PROJECTS_DIR, xslug)
 			self.path = abspath(expanduser(self.path))
 			makedirs(self.path, exist_ok = True)
-			print("Project directory " + self.slug + "created.")
+			print("Project directory " + self.slug + " created.")
 		except OSError as ex:
 			if ex.errno != errno.EEXIST:
 				print("Project directory cannot be created!")
 				raise
 
 	# Same as make_proj_dir, only it creates a subfolder in the project folder
-	def audio_seg_dir():
-		make_proj_dir()
+	def audio_seg_dir(self):
 		try:
 			self.audio_seg_path = join(self.path, "audio_seg")
 			makedirs(self.audio_seg_path, exist_ok = True)
@@ -44,8 +44,7 @@ class Project:
 				raise
 
 	# Seems to be a pattern here.
-	def transcripts_dir():
-		make_proj_dir()
+	def transcripts_dir(self):
 		try:
 			self.trans_path = join(self.path, "transcripts")
 			makedirs(self.trans_path, exist_ok = True)
@@ -59,18 +58,19 @@ class Project:
 				raise
 
 	# Easy way to create the required folders
-	def create_req_paths():
-		audio_seg_dir()
-		transcripts()
+	def create_req_paths(self):
+		self.make_proj_dir()
+		self.audio_seg_dir()
+		self.transcripts_dir()
 
 	# This will create a list of each of the transcripts in a project folder
-	def transcript_names():
+	def transcript_names(self):
 		self.trans_list = glob(join(self.trans_path, '*.json'))
 
 		
 # This will load the credentials for the Bluemix service
 def get_credentials(filename=WATSON_CREDENTIALS):
-	fullname = abspath(expanduser(filename))
-	with open(fullname, 'r') as f:
+	full_path = abspath(expanduser(filename))
+	with open(full_path, 'r') as f:
 		data = json.load(f)
 		return data['creds']
